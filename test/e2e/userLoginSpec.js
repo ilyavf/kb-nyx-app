e966d0f6fe2d659e3b084a3b22d2bc12e7fc1c17
Ilya Fadeev <ilya.fadeev@clickfree.com>
2014-03-21 23:53:57 -0400

    added e2e for login/logout

--- test/e2e/userLoginSpec.js
+++ test/e2e/userLoginSpec.js
@@ -2,8 +2,10 @@
 
 describe('Kooboodle Nyx login/logout', function() {
 
+    var url = 'http://127.0.0.1:9000/#/';
+
     beforeEach(function() {
-        browser.get('http://127.0.0.1:9000');
+        browser.get(url);
     });
 
     it('should not be able to login with wrong credentials', function() {
@@ -13,7 +15,17 @@ describe('Kooboodle Nyx login/logout', function() {
         element(by.css('button.btn-signin')).click();
 
         var loginMessage = element(by.css('.login-result'));
+        expect(browser.getCurrentUrl()).toEqual(url);
         expect(loginMessage.getText()).toEqual('User was not able to be logged in');
+    });
+
+    it('should be able to login', function() {
+        element(by.css('a.btn-login-modal')).click();
+        element(by.model('user.name')).sendKeys('ilya.fadeev@clickfree.com');
+        element(by.model('user.password')).sendKeys('123456');
+        element(by.css('button.btn-signin')).click();
+
+        expect(browser.getCurrentUrl()).toEqual(url + 'auth');
     });
 
 });
