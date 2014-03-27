b3e8162d647526fe3e76c09e0427adc91b18d91f
Ilya Fadeev <ilya.fadeev@clickfree.com>
2014-03-27 11:51:30 -0400

    karma.conf: removed link to jquery for unit tests

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
         'app/bower_components/angular-resource/angular-resource.js',
         'app/bower_components/angular-route/angular-route.js',
         'app/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
-        'app/bower_components/jquery/dist/jquery.js',
+        //'app/bower_components/jquery/dist/jquery.js',
         //'test/mock/**/*.js',
 
         // Karma creates webserver and it needs to know what files should be hosted.
