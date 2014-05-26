df208693ded99f5215f62b0eb08ed8e2d007c313
ilyavf <fadeev.ilya@gmail.com>
2014-05-26 12:18:59 -0400

    integrated single click action

--- app/scripts/pages/mykooboodle/AlbumsController.js
+++ app/scripts/pages/mykooboodle/AlbumsController.js
@@ -36,11 +36,10 @@
                     $rootScope.$broadcast('action-toolbar:selectedTotal', albumsPage.totalItems);
                 });
             });
-            $scope.selectItem = function (photoId) {
-                console.log('GALLERY ITEM CLICK: ' + photoId);
+            $scope.selectItem = function (cluster) {
+                cluster.isSelected = !cluster.isSelected;
             };
             $scope.gotoGallery = function (urlTitle) {
-                console.log('GALLERY ITEM DBLCLICK: ' + urlTitle);
                 $location.path('/auth/albums/' + urlTitle);
             };
             $scope.next = function () {
