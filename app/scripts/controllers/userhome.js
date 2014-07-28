02da982de517ffe70008b105e6c2f68dca13c7dc
ilyavf <fadeev.ilya@gmail.com>
2014-07-28 11:16:35 -0400

    styling fb item thumbs; setup toolbar for the home page

--- app/scripts/controllers/userhome.js
+++ app/scripts/controllers/userhome.js
@@ -17,15 +17,24 @@
 
         var UserHomeCtrl = function ($scope, $rootScope, $location, currentUser) {
             console.log('[UserHomeCtrl]: Initializing');
-//            if (!currentUser.isLoggedIn()) {
-//                $location.path('/');
-//                return;
-//            }
+
             currentUser.get().then(function (profile) {
                 $scope.user = profile;
             });
 
             $rootScope.$broadcast('nav:landed', 'home');
+
+            $scope.setupToolbar = function () {
+                $scope.isActionToolbarReady.then(function () {
+                    $rootScope.$broadcast('action-toolbar:config', {
+                        logo: true,
+                        title: 'Welcome to Kooboodle',
+                        logout: true,
+                        help: true
+                    });
+                });
+            };
+            $scope.setupToolbar();
         };
 
         return UserHomeCtrl;
