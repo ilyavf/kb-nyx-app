a2169cd195067d154aa9081813389ad6a2ea522f
ilyavf <fadeev.ilya@gmail.com>
2014-08-21 17:17:47 -0400

    arrow nav for calendar: check if arrows are disabled

--- app/scripts/directives/ArrowNavigatorDir.js
+++ app/scripts/directives/ArrowNavigatorDir.js
@@ -18,9 +18,9 @@
             return {
                 restrict: 'E',
                 template:
-                    '<a class="arrows" ng-click="prev()"><span class="left">&nbsp;</span></a>' +
+                    '<a class="arrows" ng-click="prev()"><span class="left {{leftDisabled}}">&nbsp;</span></a>' +
                     '    <span class="title">{{currentItem}}</span>' +
-                    '<a class="arrows" ng-click="next()"><span class="right">&nbsp;</span></a>',
+                    '<a class="arrows" ng-click="next()"><span class="right {{rightDisabled}}">&nbsp;</span></a>',
                 transclude: true,
                 scope: {
                     'items': '=',
@@ -33,6 +33,8 @@
                     scope.currentIndex = scope.initIndex || 0;
                     scope.currentItem = scope.items[scope.currentIndex];
 
+                    scope.leftDisabled = scope.rightDisabled = 'disabled';
+
                     scope.next = function () {
                         if (++scope.currentIndex > scope.items.length-1) {
                             scope.currentIndex = scope.items.length-1 ;
@@ -40,6 +42,7 @@
                             scope.currentItem = scope.items[scope.currentIndex];
                             scope.onNext(scope.currentItem);
                         }
+                        scope.checkArrows(scope.items.length, scope.currentIndex);
                     };
                     scope.prev = function () {
                         if (--scope.currentIndex < 0) {
@@ -48,12 +51,18 @@
                             scope.currentItem = scope.items[scope.currentIndex];
                             scope.onPrev(scope.currentItem);
                         }
+                        scope.checkArrows(scope.items.length, scope.currentIndex);
                     };
                     scope.control.setItems = function (items, index) {
                         scope.items = items;
                         scope.currentIndex = index || 0;
                         scope.currentItem = scope.items[scope.currentIndex];
-                    }
+                        scope.checkArrows(items.length, index);
+                    };
+                    scope.checkArrows = function (itemsTotal, index) {
+                        scope.rightDisabled = index < itemsTotal-1 ? '' : 'disabled';
+                        scope.leftDisabled = index > 0 ? '' : 'disabled';
+                    };
                 }
             }
         };
